17af676efd93637cc53be7488346de4d87206b35
zongxili <zongxiliwork@gmail.com>
2019-09-30 16:51:26 -0700

    finished the setup

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import ReactDOM from "react-dom";
 import "index.scss";
 
 import Application from "components/Application";
+import tweets from './tweets.json';
 
 ReactDOM.render(<Application />, document.getElementById("root"));
--- stories/index.js
+++ stories/index.js
@@ -1,24 +1,38 @@
+/* We need to import React because we are using JSX. */
 import React from "react";
 
+/* The storiesOf function from the React version of Storybook is used to start a series of stories. */
 import { storiesOf } from "@storybook/react";
+
+/* The action function from the Storybook actions addon lets verify that our component triggers event handlers correctly. */
 import { action } from "@storybook/addon-actions";
 
-import "index.scss";
+/* Storybook is a test environment. We need to import the component that we are testing. */
+import Task from "./Task";
 
-import Button from "components/Button";
+/* This is fake data that we can pass as a prop to each version of our Task. */
+export const task = {
+  id: "1",
+  title: "Test Task",
+  state: "TASK_INBOX",
+  updatedAt: new Date(2018, 0, 1, 9, 0)
+};
 
-storiesOf("Button", module)
-  .addParameters({
-    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
-  })
-  .add("Base", () => <Button>Base</Button>)
-  .add("Confirm", () => <Button confirm>Confirm</Button>)
-  .add("Danger", () => <Button danger>Cancel</Button>)
-  .add("Clickable", () => (
-    <Button onClick={action("button-clicked")}>Clickable</Button>
+/* We pass these actions below using the spread operator */
+export const actions = {
+  onPinTask: action("onPinTask"),
+  onArchiveTask: action("onArchiveTask")
+};
+
+/* We start a sequence of stories, but calling the imported `storiesOf` function. */
+storiesOf("Task", module)
+  /* Our first story, called 'default', renders a basic Task with both actions. */
+  .add("default", () => <Task task={task} {...actions} />)
+  /* Our second story, called 'pinned', clones the task, and sets its state to "TASK_PINNED". */
+  .add("pinned", () => (
+    <Task task={{ ...task, state: "TASK_PINNED" }} {...actions} />
   ))
-  .add("Disabled", () => (
-    <Button disabled onClick={action("button-clicked")}>
-      Disabled
-    </Button>
+  /* Our third story, called 'archived', clones the task, and sets its state to "TASK_ARCHIVED". */
+  .add("archived", () => (
+    <Task task={{ ...task, state: "TASK_ARCHIVED" }} {...actions} />
   ));
