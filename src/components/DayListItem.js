d11b53f6e586d5a7df79bb7c892b9f4621cb5c35
zongxili <zongxiliwork@gmail.com>
2019-09-30 19:48:02 -0700

    finished the first 2 task

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,56 @@
 import React from "react";
 
-export default function DayListItem(props) {
+import "components/DayListItem.scss"
+
+import classNames from "classnames/bind";
+
+// export default function DayListItems(props) {
+
+//   const formatSpots = (spot) => {
+//     if (!spot) return "no spots remaining";
+//     if (spot === 1) return "1 spot remaining";
+//     else return `${spot} spots remaining`;
+//   };
+
+//   const dayClass = classNames("day-list__item", {
+//     "day-list__item--selected": props.selected,
+//     "day-list__item--full": !props.spots
+//   });
+
+//   return (
+//     <li 
+//     className={dayClass}
+//     onClick={() => props.setDay(props.name)}
+//     >
+//       <h2 className="text--regular">{props.name}</h2>
+//       <h3 className="text--light">{formatSpots(props.spots)}</h3>
+//     </li>
+//   );
+// }
+
+
+export default function DayListItems(props) {
+
+  // check the object and add the class names
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots
+  });
+
+  // format props.spots to make tests pass
+  const formatSpots = function(spots){
+    if (spots === 0)
+      return "no spots remaining"
+    else if (spots === 1)
+      return "1 spot remaining"
+    else 
+      return `${spots} spots remaining`
+  };
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
--- stories/index.js
+++ stories/index.js
@@ -1,38 +1,36 @@
-/* We need to import React because we are using JSX. */
 import React from "react";
 
-/* The storiesOf function from the React version of Storybook is used to start a series of stories. */
 import { storiesOf } from "@storybook/react";
-
-/* The action function from the Storybook actions addon lets verify that our component triggers event handlers correctly. */
 import { action } from "@storybook/addon-actions";
 
-/* Storybook is a test environment. We need to import the component that we are testing. */
-import Task from "./Task";
+import "index.scss";
 
-/* This is fake data that we can pass as a prop to each version of our Task. */
-export const task = {
-  id: "1",
-  title: "Test Task",
-  state: "TASK_INBOX",
-  updatedAt: new Date(2018, 0, 1, 9, 0)
-};
+import Button from "components/Button";
+import DayListItem from "components/DayListItem";
 
-/* We pass these actions below using the spread operator */
-export const actions = {
-  onPinTask: action("onPinTask"),
-  onArchiveTask: action("onArchiveTask")
-};
-
-/* We start a sequence of stories, but calling the imported `storiesOf` function. */
-storiesOf("Task", module)
-  /* Our first story, called 'default', renders a basic Task with both actions. */
-  .add("default", () => <Task task={task} {...actions} />)
-  /* Our second story, called 'pinned', clones the task, and sets its state to "TASK_PINNED". */
-  .add("pinned", () => (
-    <Task task={{ ...task, state: "TASK_PINNED" }} {...actions} />
+storiesOf("Button", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Base", () => <Button>Base</Button>)
+  .add("Confirm", () => <Button confirm>Confirm</Button>)
+  .add("Danger", () => <Button danger onClick={action("button-clicked")}>Cancel</Button>)
+  .add("Clickable", () => (
+    <Button onClick={action("button-clicked")}>Clickable</Button>
   ))
-  /* Our third story, called 'archived', clones the task, and sets its state to "TASK_ARCHIVED". */
-  .add("archived", () => (
-    <Task task={{ ...task, state: "TASK_ARCHIVED" }} {...actions} />
+  .add("Disabled", () => (
+    <Button disabled onClick={action("button-clicked")}>
+      Disabled
+    </Button>
+  ));
+
+  storiesOf("DayListItem", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Unselected", () => <DayListItem name="Monday" spots={5} />)
+  .add("Selected", () => <DayListItem name="Monday" spots={5} selected />) 
+  .add("Full", () => <DayListItem name="Monday" spots={0} />) 
+  .add("Clickable", () => (
+    <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} />
   ));
