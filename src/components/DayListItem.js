a531081060508617240c887f00cf05dd89b46733
zongxili <zongxiliwork@gmail.com>
2019-09-30 21:32:01 -0700

    finished the forth task

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,31 +4,6 @@ import "components/DayListItem.scss"
 
 import classNames from "classnames/bind";
 
-// export default function DayListItems(props) {
-
-//   const formatSpots = (spot) => {
-//     if (!spot) return "no spots remaining";
-//     if (spot === 1) return "1 spot remaining";
-//     else return `${spot} spots remaining`;
-//   };
-
-//   const dayClass = classNames("day-list__item", {
-//     "day-list__item--selected": props.selected,
-//     "day-list__item--full": !props.spots
-//   });
-
-//   return (
-//     <li 
-//     className={dayClass}
-//     onClick={() => props.setDay(props.name)}
-//     >
-//       <h2 className="text--regular">{props.name}</h2>
-//       <h3 className="text--light">{formatSpots(props.spots)}</h3>
-//     </li>
-//   );
-// }
-
-
 export default function DayListItems(props) {
 
   // check the object and add the class names
@@ -40,11 +15,11 @@ export default function DayListItems(props) {
   // format props.spots to make tests pass
   const formatSpots = function(spots){
     if (spots === 0)
-      return "no spots remaining"
+      return "no spots remaining";
     else if (spots === 1)
-      return "1 spot remaining"
-    else 
-      return `${spots} spots remaining`
+      return "1 spot remaining";
+    else
+      return `${spots} spots remaining`;
   };
 
   return (
--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import ReactDOM from "react-dom";
 import "index.scss";
 
 import Application from "components/Application";
-import tweets from './tweets.json';
+// import tweets from './tweets.json';
 
 ReactDOM.render(<Application />, document.getElementById("root"));
