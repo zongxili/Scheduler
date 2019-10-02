b72e357727901626a68270a869876102c9154488
zongxili <zongxiliwork@gmail.com>
2019-10-01 20:44:30 -0700

    changed the dependencies

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,4 @@
-import React, { useState, Fragment, useEffect } from "react";
+import React, { useState, Fragment } from "react";
 import DayList from "./DayList";
 import Appointment from "./Appointment/index";
 import "components/Application.scss";
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import "./styles.scss";
 // Import the Header, Show and Empty 
 import Show from "./Show";
