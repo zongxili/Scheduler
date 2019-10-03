b80981fcee2952f5d5c5cfb132be54927a3122c9
zongxili <zongxiliwork@gmail.com>
2019-10-02 20:50:06 -0700

    refactored code

--- src/components/Application.js
+++ src/components/Application.js
@@ -13,21 +13,6 @@ export default function Application(props) {
     interviewers: {}
   });
 
-  // const appointments = getAppointmentsForDay(state, day);
-
-  // const schedule = appointments.map((appointment) => {
-  //   const interview = getInterview(state, appointment.interview);
-
-  //   return (
-  //     <Appointment
-  //       key={appointment.id}
-  //       id={appointment.id}
-  //       time={appointment.time}
-  //       interview={getInterview(state, appointment.interview)}
-  //     />
-  //   );
-  // });
-
   const setDay = day => setState({ ...state, day });
   const setDays = days => setState(prev => ({ ...prev, days }));
 
@@ -41,12 +26,9 @@ export default function Application(props) {
 
     Promise.all([promiseDays, promiseAppointments, promiseInterviewers]).then(function(values) {
       setState(prev => ({...prev, days: values[0].data, appointments: values[1].data, interviewers: values[2].data}));
-
-      // setDays(values[0].data);
       console.log(values);
     });
   },[]);
-
 
   return (
     <main className="layout">
